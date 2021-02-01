e943b333c88179caa6a07ded3246e2f1644d8e83
Jeremy <jeremy1nz2000@gmail.com>
2021-02-02 11:13:55 +1300

    Merge of home page changes made by Natalya with main branch

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,20 +1,14 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
-import * as PropTypes from "prop-types";
+import "./Home.css";
 
-class Button extends React.Component {
-    render() {
-        return null;
-    }
-}
 
-Button.propTypes = {
-    variant: PropTypes.string,
-    children: PropTypes.node
-};
 
-export class Home extends React.Component{
+export class Home extends React.Component {
+
 
     onChange;
 
@@ -22,70 +16,123 @@ export class Home extends React.Component{
         return undefined;
     }
 
-    render(){
-        return(
-            <div>
+    render() {
+        return (
 
-                <h1>Welcome to Our Fabulous Database!</h1>
+            <div className="ibm-home-div">
+                <div className="col-lg-12 text-center">
 
-                <div className="row">
-                    <div className="col-lg-6">
-                        <label>Search By:</label>
-
-                        <div className="input-group rounded">
-                            <input type="search" className="form-control rounded" placeholder="Put any related tags..."
-                                   aria-label="Search"
-                                   aria-describedby="search-addon"/>
-                            <span className="input-group-text border-0" id="search-addon">
-                            <i className="fas fa-search"></i>
-                            </span>
-                        </div>
-
+                    <br/><br/>
+                </div>
+                <div className="container">
+                    <div className="input-group">
+                        <input type="search" className="form-control" placeholder="Search for case studies" aria-label="Search"
+                               aria-describedby="search-addon"/>
+                        <button type="button" className="btn btn-primary">search</button>
                     </div>
-                        <div className="col-lg-3">
+
+                    <div className="row justify-content-evenly">
+                        <div className="col-sm-3">
 
                             <div className="control-group form-group">
                                 <div className="controls">
-                                    <label>Project Industry:</label>
-                                    <select id="industry" name="industry" className="form-control" required
-                                            data-validation-required-message="select an Industry">
-                                        <option disabled selected value="0">-- Select an option--</option>
+                                    <select id="Project_industry" name="industry" className="form-control" required
+                                            onChange={event => {
+                                                this.setState({project_industry: event.target.value})
+                                            }}>
+                                        <option disabled selected value="0">All Industries</option>
                                         <option value="banking">Banking</option>
                                         <option value="healthcare"> Heath Care</option>
                                         <option value="telecom">Telecommunication</option>
                                         <option value="other">Other</option>
                                     </select>
                                 </div>
-
                             </div>
-
                         </div>
+                        <div className="col-sm-3">
 
-                        <div className="col-lg-3">
                             <div className="control-group form-group">
                                 <div className="controls">
-                                    <label>Project Type:</label>
-                                    <select id="industry" name="industry" className="form-control" required
-                                            data-validation-required-message="select an Industry">
-                                        <option disabled selected value="0">-- Select an option--</option>
-                                        <option value="agile">Agile Methodology</option>
-                                        <option value="integration"> Cloud Integration</option>
-                                        <option value="interface">Creating GUI</option>
-                                        <option value="webapp">Building Web Application</option>
+                                    <select id="Project_industry" name="industry" className="form-control" required
+                                            onChange={event => {
+                                                this.setState({project_industry: event.target.value})
+                                            }}>
+                                        <option disabled selected value="0">All Companies</option>
+                                        <option value="banking"> ANZ</option>
+                                        <option value="healthcare"> Wellington Hospital</option>
+                                        <option value="telecom"> Vodafone</option>
+                                        <option value="other"> Other</option>
                                     </select>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-lg-12 text-center">
-                            <div>
-                                <button className="btn btn-info" id="search-form-btn" onClick={() => this.searchCaseStudy()}> SEARCH CASE STUDY</button>
-                            </div>
-                        </div>
                     </div>
+
+                    <div>
+                        <br></br>
+                        <div className="col-lg-12 text-center">
+                            <h6>Or</h6>
+                            <button className="btn btn-primary" type="submit">View all</button>
+                        </div>
+
+
+                    </div>
+
+
+
+
+
                 </div>
-            )
+            </div>
+
+
+
+        )
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
